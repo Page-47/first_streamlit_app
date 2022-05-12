4670e6f370ec7fa6f42bc550321d264dd42fb7a9
Jay White <105438284+Page-47@users.noreply.github.com>
2022-05-12 12:40:34 -0400

    Update streamlit_app.py
    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
-streamlit.title("my parentes new healthy menu")
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.test('Hard-Boiled Free-Range Egg')
